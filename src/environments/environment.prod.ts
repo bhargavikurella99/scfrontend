9c3b06c3158d8738e42dcaf0c4b4a052142fca24
Kurella <bhargavi.kurella@capgemini.com>
2022-05-27 16:30:07 +0530

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  restPathRoot: 'http://localhost:8081/demo-server/',
+  restServiceRoot: 'http://localhost:8081/demo-server/services/rest/',
+  security: 'jwt',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  restPathRoot: 'http://localhost:8081/',
+  restServiceRoot: 'http://localhost:8081/services/rest/',
+  security: 'jwt',
+};
